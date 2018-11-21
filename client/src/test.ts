af716a15fd8f2ac6ffe4c6e12ed23327763806df
Gennady Malyshev <malysheg@gmail.com>
2018-11-21 06:57:47 -0600

    tweaks to get client tests passing again

--- client/src/test.ts
+++ client/src/test.ts
@@ -2,7 +2,7 @@
 // import * as Mocha from 'mocha'
 import * as assert from 'assert';
 import './physics/PhysicsManager.tests';
-import './math2d/Point2d.tests';
+import '../../shared/math2d/Point2d.tests';
 // export * from './physics/PhysicsManager.tests';
 // import physManager = import('./physics/PhysicsManager.tests');
 //import {} from './physics/PhysicsManager.tests'
